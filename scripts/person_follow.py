df9c5c78276bda7ac56537473fbcba662cf76498
zk794 <zoa.katok@gmail.com>
2022-04-09 19:53:33 -0500

    idk

--- scripts/person_follow.py
+++ scripts/person_follow.py
@@ -10,35 +10,32 @@ from geometry_msgs.msg import Vector3
 # minimum distance from person
 followDist = 0.3
 
-class FollowPerson(object):
-​
+class FollowPerson():
+
     def __init__(self):
         # Start rospy node.
         rospy.init_node("follow_person")
-​
+
         # Declare our node as a subscriber to the scan topic and
         #   set self.process_scan as the function to be used for callback.
         rospy.Subscriber("/scan", LaserScan, self.process_scan)
-​
+
         # Get a publisher to the cmd_vel topic.
         self.twist_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-​
+
         # Create a default twist msg (all values 0).
         lin = Vector3()
         ang = Vector3()
         self.twist = Twist(linear=lin,angular=ang)
-​
+
     def process_scan(self, data):
         # Determine closeness to person by looking at scan data from in front of
         #   the robot, set linear velocity based on that information, and
         #   publish to cmd_vel.
-​
+
         # The ranges field is a list of 360 number where each number
         #   corresponds to the distance to the closest obstacle from the
         #   LiDAR at various angles. Each measurement is 1 degree apart.
-​
-        # The first entry in the ranges list corresponds with what's directly
-        #   in front of the robot.
 
         # find closest object's angle and distance
         closestInd = 0
@@ -76,12 +73,11 @@ class FollowPerson(object):
 
         # Publish msg to cmd_vel.
         self.twist_pub.publish(self.twist)
-​
-​
+
     def run(self):
         # Keep the program alive.
         rospy.spin()
-​
+        
 if __name__ == '__main__':
     # Declare a node and run it.
     node = FollowPerson()
